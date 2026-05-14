4139a982d502583791321083deab1d1f8d51379c
ELFarri <mouatazfarri@gmail.com>
2026-05-14 02:01:38 +0200

    Finish dynamic meals implementation in MealsScreen

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import '../app_theme.dart';
 import '../widgets/mediterranean_diet_view.dart';
 import '../widgets/meals_list_view.dart';
 import '../widgets/title_view.dart';
 import 'nutrition/food_search_screen.dart';
+import '../providers/meal_provider.dart';
+import '../providers/user_provider.dart';
 
 class MealsScreen extends StatefulWidget {
   final AnimationController? animationController;
@@ -16,14 +19,12 @@ class MealsScreen extends StatefulWidget {
 
 class _MealsScreenState extends State<MealsScreen> with TickerProviderStateMixin {
   late AnimationController _localAnimationController;
-  List<Widget> _listViews = [];
 
   @override
   void initState() {
     super.initState();
     _localAnimationController = AnimationController(
         duration: const Duration(milliseconds: 1000), vsync: this);
-    _addAllListData();
     _localAnimationController.forward();
   }
 
@@ -33,73 +34,62 @@ class _MealsScreenState extends State<MealsScreen> with TickerProviderStateMixin
     super.dispose();
   }
 
-  void _addAllListData() {
-    _listViews.add(
-      TitleView(
-        titleText: 'Nutrition Summary',
-        subText: 'Details',
-        animationController: _localAnimationController,
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: _localAnimationController,
-            curve: Interval((1 / 5) * 0, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-      ),
-    );
-
-    _listViews.add(
-      MediterraneanDietView(
-        animationController: _localAnimationController,
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: _localAnimationController,
-            curve: Interval((1 / 5) * 1, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-        eatenCalories: 1250,
-        burnedCalories: 350,
-        targetCalories: 2000,
-      ),
-    );
-
-    _listViews.add(
-      TitleView(
-        titleText: 'Daily Meals',
-        subText: 'Add Food',
-        animationController: _localAnimationController,
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: _localAnimationController,
-            curve: Interval((1 / 5) * 2, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-      ),
-    );
-
-    _listViews.add(
-      MealsListView(
-        animationController: _localAnimationController,
-        animation: Tween<double>(begin: 0.0, end: 1.0).animate(
-          CurvedAnimation(
-            parent: _localAnimationController,
-            curve: Interval((1 / 5) * 3, 1.0, curve: Curves.fastOutSlowIn),
-          ),
-        ),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    final mealProvider = context.watch<MealProvider>();
+    final userProvider = context.watch<UserProvider>();
+
     return Container(
       color: AppTheme.background,
       child: Stack(
         children: [
-          ListView.builder(
+          ListView(
             padding: EdgeInsets.fromLTRB(0, AppBar().preferredSize.height + MediaQuery.of(context).padding.top + 8, 0, 100),
-            itemCount: _listViews.length,
-            itemBuilder: (context, index) => _listViews[index],
+            children: [
+              TitleView(
+                titleText: 'Nutrition Summary',
+                subText: 'Details',
+                animationController: _localAnimationController,
+                animation: Tween<double>(begin: 0.0, end: 1.0).animate(
+                  CurvedAnimation(
+                    parent: _localAnimationController,
+                    curve: Interval((1 / 5) * 0, 1.0, curve: Curves.fastOutSlowIn),
+                  ),
+                ),
+              ),
+              MediterraneanDietView(
+                animationController: _localAnimationController,
+                animation: Tween<double>(begin: 0.0, end: 1.0).animate(
+                  CurvedAnimation(
+                    parent: _localAnimationController,
+                    curve: Interval((1 / 5) * 1, 1.0, curve: Curves.fastOutSlowIn),
+                  ),
+                ),
+                eatenCalories: mealProvider.totalCalories,
+                burnedCalories: 0,
+                targetCalories: userProvider.targetCalories,
+              ),
+              TitleView(
+                titleText: 'Daily Meals',
+                subText: 'Add Food',
+                animationController: _localAnimationController,
+                animation: Tween<double>(begin: 0.0, end: 1.0).animate(
+                  CurvedAnimation(
+                    parent: _localAnimationController,
+                    curve: Interval((1 / 5) * 2, 1.0, curve: Curves.fastOutSlowIn),
+                  ),
+                ),
+              ),
+              MealsListView(
+                animationController: _localAnimationController,
+                animation: Tween<double>(begin: 0.0, end: 1.0).animate(
+                  CurvedAnimation(
+                    parent: _localAnimationController,
+                    curve: Interval((1 / 5) * 3, 1.0, curve: Curves.fastOutSlowIn),
+                  ),
+                ),
+              ),
+            ],
           ),
           _getAppBarUI(),
           _buildFloatingAddButton(),
